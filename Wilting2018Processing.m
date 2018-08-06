4686947d2fd044157e71860c9510bb30ee9a2f8e
Michael Chang <mick.chang@mail.utoronto.ca>
2018-08-05 20:57:02 -0400

    Wilting Codes
    This commit is just before I remove all the seizure detection features from the Wilting analysis script

---
+++ Wilting2018Processing.m
@@ -0,0 +1,36 @@
+%Program: Epileptiform Activity Detector 
+%Author: Michael Chang (michael.chang@live.ca) and Fred Chen; 
+%Copyright (c) 2018, Valiante Lab
+%Version 1.0
+
+
+%% Clear All
+
+close all
+clear all
+clc
+
+%% Load .abf and excel data
+
+    [FileName,PathName] = uigetfile ('*.abf','pick .abf file', 'F:\');%Choose abf file
+    [x,samplingInterval,metadata]=abfload([PathName FileName]); %Load the file name with x holding the channel data(10,000 sampling frequency) -> Convert index to time value by dividing 10k
+                                                                                         
+%% create time vector
+frequency = 1000000/samplingInterval; %Hz. si is the sampling interval in microseconds from the metadata
+t = (0:(length(x)- 1))/frequency;
+t = t';
+
+%% Seperate signals from .abf files
+LFP = x(:,1); 
+LED = x(:,2); 
+
+%% normalize the LFP data
+LFP_normalized = LFP - LFP(1);
+
+%% Data Processing 
+%Bandpass butter filter [1 - 100 Hz]
+[b,a] = butter(2, [[1 100]/(frequency/2)], 'bandpass');
+LFP_normalizedFiltered = filtfilt (b,a,LFP_normalized);
+
+%%Write to .csv (row)
+csvwrite('FileName.dat', LFP')
