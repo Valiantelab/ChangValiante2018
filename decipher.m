85528303a1344d5e870f37443d94f585794177e8
mchang58 <37356279+mchang58@users.noreply.github.com>
2018-10-09 03:34:25 -0400

    checkpoint - V6.6
    I've updated the Quinary Classifier and it's running smoothly.
    
    need to confirm the classifier is working well though.

--- decipher.m
+++ decipher.m
@@ -1,44 +1,43 @@
 function [label,classification] = decipher (events, i)
 %decipher is a function to convert my number classification system into
-%normal language
+%normal language. Author: Michael Chang, Liam Long, and Thomas Lordello
 %   This function turns the numbers into words for the purposes of
 %   labelling the epileptiform events that are plotted for troubleshooting
 %   and demonstration purposes. The numbers are classification codes for
 %   the type of epileptiform event and if tonic-phase is present.
 
 %% Label the epiletiform event (for plotting); convert numbers to words
-if events(i,7) == 1 
-    label = 'SLE';
-else if events(i,7) == 2
+switch events(i,7) 
+    case 0
+        label = 'Review Event';
+    case 1 
+        label = 'SLE';
+    case 2
         label = 'IIE';
-    else if events(i,7) == 3
-            label = 'IIS';
-        else if events(i,7) == 4
-                label = 'artifact';
-            else if events (i,7) == 1.5
-                label = 'Questionable SLE';
-                else if events (i,7) == 2.5
-                        label = 'Your hypothesis failed: IIS w/ tonic';
-                    else                                    
-                    label = 'error; review the function decipher.m to see if you missed a class of epileptiform events';
-                    end                                
-                end                                                    
-            end
-        end
-    end
-end  
+    case 3
+        label = 'IIS';
+    case 4
+        label = 'artifact';
+    case 1.5
+        label = 'Questionable SLE';
+    otherwise                                    
+        label = 'Error: review the function decipher.m to see if you missed a class of epileptiform events';
+end
+
+
 
 %% classification of the epileptiform event based on its tonic-phase         
-if events(i,15) == 0
-classification = 'No tonic phase';
-else if events(i,15) == 1
+switch events(i,13) 
+    case -1
+        classification = 'IIS (duration <3 s)';
+    case 0
+        classification = 'No tonic phase';
+    case 1
         classification = 'Tonic-Clonic SLE';
-    else if events(i,15) == 2
-            classification = 'Tonic-only SLE';
-        else
-            classification = 'IIS (duration <3 s)';
-        end
-    end
+    case 2
+        classification = 'Tonic-only SLE';
+    otherwise
+        classification = 'Error: review the function decipher.m; perhaps you discovered a new ictal phase ';
 end   
 
 
